73ee873161c5d7e2377cea33f75cf13cc630cc50
lbbento <lucasbento7@gmail.com>
2017-04-15 15:24:21 +1000

    Fixed presenter test after changing the way the view is atttached

--- app/src/test/java/com/lbbento/daydreamnasa/presenter/BasePresenterTest.kt
+++ app/src/test/java/com/lbbento/daydreamnasa/presenter/BasePresenterTest.kt
@@ -1,23 +1,22 @@
 package com.lbbento.daydreamnasa.presenter
 
 import com.lbbento.daydreamnasa.view.BaseViewContract
-import com.nhaarman.mockito_kotlin.eq
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.verify
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
 class BasePresenterTest {
 
     @Test
     fun onAttachedToWindowShouldSetViewAndCallOnSetContent() {
-        val mockedView = mock<BaseViewContract> {}
+        val mockedView = mock(BaseViewContract::class.java)
         val basePresenter = BasePresenterTest()
 
         basePresenter.onAttachedToWindow(mockedView)
 
         verify(mockedView).setScreenContent()
-        assertEquals(basePresenter.mView, eq(mockedView))
+        assertEquals(basePresenter.mView, mockedView)
     }
 
     class BasePresenterTest : BasePresenter<BaseViewContract>()
