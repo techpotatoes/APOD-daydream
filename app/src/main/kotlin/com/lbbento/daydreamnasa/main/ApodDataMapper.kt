b152ddd33b0a085042aaa7c4d4b4ded8f0311e13
lbbento <lucasbento7@gmail.com>
2017-04-18 20:34:47 +1000

    Added youtube image thumbnail.

--- app/src/main/kotlin/com/lbbento/daydreamnasa/main/ApodDataMapper.kt
+++ app/src/main/kotlin/com/lbbento/daydreamnasa/main/ApodDataMapper.kt
@@ -1,14 +1,45 @@
 package com.lbbento.daydreamnasa.main
 
 import com.lbbento.daydreamnasa.data.api.apod.ApodDTO
+import java.lang.String.format
+import java.net.MalformedURLException
+import java.net.URL
 import javax.inject.Inject
 import javax.inject.Singleton
 
+
 @Singleton
 open class ApodDataMapper @Inject constructor() {
+    val APOD_MEDIA_TYPE_VIDEO = "video"
 
     fun apodDTOToMainDaydreamViewModel(apodDTO: ApodDTO) : MainDaydreamServiceViewModel{
-        return MainDaydreamServiceViewModel(imageUrl = apodDTO.hdurl, title = apodDTO.title, description = apodDTO.explanation)
+        return MainDaydreamServiceViewModel(imageUrl = parseMedia(apodDTO.media_type, apodDTO.hdurl), title = apodDTO.title, description = apodDTO.explanation)
+    }
+
+    private fun parseMedia(mediaType: String, hdurl: String): String {
+        when(mediaType) {
+            APOD_MEDIA_TYPE_VIDEO -> { return extractThumbnail(hdurl) }
+        }
+        return hdurl
+    }
+
+    private fun extractThumbnail(hdurl: String): String {
+        if (hdurl.contains("youtube")) {
+            return format("https://img.youtube.com/vi/%s/hqdefault.jpg", extractYoutubeId(hdurl))
+        }
+        return ""
+    }
+
+    @Throws(MalformedURLException::class)
+    private fun extractYoutubeId(url: String): String {
+        val query = URL(url).getQuery()
+        val param = query.split("&".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()
+        var id: String = ""
+        param
+                .map { row -> row.split("=".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray() }
+                .filter { it[0] == "v" }
+                .forEach { id = it[1] }
+        return id
     }
 
 }
---
+++ app/src/test/java/com/lbbento/daydreamnasa/main/ApodDataMapperTest.kt
@@ -0,0 +1,34 @@
+package com.lbbento.daydreamnasa.main
+
+import com.lbbento.daydreamnasa.data.api.apod.ApodDTO
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import java.util.*
+
+@RunWith(MockitoJUnitRunner::class)
+class ApodDataMapperTest {
+
+    val APOD_DTO = ApodDTO(date = Date(), explanation = "desc", hdurl = "url", copyright = "", media_type = "", service_version = "", title = "title", url = "")
+    val MAIN_DREAM_VIEW_MODEL = MainDaydreamServiceViewModel(imageUrl = "url", description = "desc", title = "title")
+
+    val APOD_DTO_YOUTUBE = ApodDTO(date = Date(), explanation = "desc", hdurl = "https://www.youtube.com/watch?v=Q3VjaCy5gck", copyright = "", media_type = "video", service_version = "", title = "title", url = "")
+    val MAIN_DREAM_VIEW_MODEL_YOUTUBE = MainDaydreamServiceViewModel(imageUrl = "https://img.youtube.com/vi/Q3VjaCy5gck/hqdefault.jpg", description = "desc", title = "title")
+
+
+    @Test
+    fun shouldConvertapodDTOToMainDaydreamViewModel() {
+        val apodDataMapper = ApodDataMapper()
+
+        assertEquals(apodDataMapper.apodDTOToMainDaydreamViewModel(APOD_DTO), MAIN_DREAM_VIEW_MODEL)
+    }
+
+    @Test
+    fun shouldGetYoutubeThumbnailIfMediaTypeIsVideo() {
+        val apodDataMapper = ApodDataMapper()
+
+        assertEquals(apodDataMapper.apodDTOToMainDaydreamViewModel(APOD_DTO_YOUTUBE), MAIN_DREAM_VIEW_MODEL_YOUTUBE)
+    }
+}
